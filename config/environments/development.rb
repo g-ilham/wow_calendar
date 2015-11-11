16c18f31f3035ba54e77b66dee5042286395e48a
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-11-11 17:31:00 +0300

    [Config] added debug queries in log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,10 +12,8 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -26,6 +24,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.log_level = :debug
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
