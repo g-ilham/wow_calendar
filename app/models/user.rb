a5fdb4fd3c49f56b51fef19b3baf82088e844271
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-11-16 15:36:02 +0300

    [Authorization] added styles for not valid column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  begin :validations
+    validates :first_name, :last_name,
+      length: { minimum: 2, maximum: 100 },
+      format: { with: /[а-яА-Яa-zA-Z]+/ },
+      allow_nil: true,
+      allow_blank: true
+  end
 end
