0c8bac999f12ee0705c096db035e551593935e12
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-11-17 13:47:40 +0300

    [Google recaptcha] correcting enviroments

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = ENV['RECAPTCHA_PUBLIC_KEY']
-  config.private_key = ENV['RECAPTCHA_PRIVATE_KEY']
+  config.public_key  = ENV['WOWCALENDAR_RECAPTCHA_PUBLIC_KEY']
+  config.private_key = ENV['WOWCALENDAR_RECAPTCHA_PRIVATE_KEY']
   config.api_version = 'v2'
 end
