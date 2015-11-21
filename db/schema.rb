335fc8cc6f6a7943a68a459e76c44652d314accb
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-11-21 11:47:33 +0300

    [Events] updating db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119130351) do
+ActiveRecord::Schema.define(version: 20151120133600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
-    t.integer  "starts_at",                  null: false
-    t.integer  "ends_at"
     t.integer  "user_id"
     t.boolean  "all_day",    default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.datetime "starts_at"
+    t.datetime "ends_at"
   end
 
   create_table "users", force: :cascade do |t|
