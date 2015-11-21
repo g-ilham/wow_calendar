6f014294dbbfe2ceafb03bb731e65d53ed72cd6e
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-11-21 11:48:23 +0300

    [Events] corrected event serializer for events collection

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,11 +2,11 @@ class EventSerializer < ActiveModel::Serializer
   attributes :id, :start, :end, :allDay, :title
 
   def start
-    object.starts_at.to_s
+    object.starts_at
   end
 
   def end
-    object.ends_at.to_s
+    object.ends_at
   end
 
   def allDay
