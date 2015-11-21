00c93f9afb5e38bf7043e5bc20bb2708e559038a
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-11-21 11:44:13 +0300

    [Events | Create \ Update] added validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+
+  begin :validations
+    validates :title, format: { with: /[а-яА-Яa-zA-Z]+/ }, if: 'self.title.present?'
+    validates :title, length: { minimum: 2, maximum: 100 }
+    validates_datetime :starts_at, on_or_after: :today
+    validates_datetime :ends_at, on_or_after: :starts_at, unless: "self.all_day?"
+  end
 end
