05c9a32fc8bfb572dc8ec043bedb14a8d47649e2
g-ilham <ilgamgaysin@gmail.com>
2015-11-28 11:49:06 +0300

    [DB Migrations] fixed wrong migration with user email

--- db/migrate/20151113085933_change_email_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeEmailColumnToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :email, :string, null: false, default: ""
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20151123155551) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email",                  default: ""
+    t.string   "email"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.string   "encrypted_password",     default: "",    null: false
