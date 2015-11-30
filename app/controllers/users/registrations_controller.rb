529a9937d4a4d4d8160e657869ba1d7922d22062
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-11-30 13:05:31 +0300

    [Codeclimate | Registration] refactoring issue

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,21 +13,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     if resource.valid?
       if verify_recaptcha
-        self.valid_captcha = true
-        resource.save
-        yield resource if block_given?
-        if resource.persisted?
-          persisted_resoucre_code
-        else
-          clean_up_passwords resource
-          set_minimum_password_length
-          respond_with resource do |format|
-            format.js
-          end
-        end
+        success_captcha
       else
         not_valid_captcha_response
       end
+    end
+  end
+
+  def success_captcha
+    self.valid_captcha = true
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      persisted_resoucre_code
+    else
+      resource_blank_response
     end
   end
 
@@ -41,17 +41,33 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def persisted_resoucre_code
     if resource.active_for_authentication?
-      set_flash_message :notice, :signed_up if is_flashing_format?
-      sign_up(resource_name, resource)
-      respond_with resource, location: after_sign_up_path_for(resource)do |format|
-        format.js
-      end
+      active_authentication
     else
-      set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-      expire_data_after_sign_in!
-      respond_with resource, location: after_inactive_sign_up_path_for(resource) do |format|
-        format.js
-      end
+      not_active_authentication
+    end
+  end
+
+  def resource_blank_response
+    clean_up_passwords resource
+    set_minimum_password_length
+    respond_with resource do |format|
+      format.js
+    end
+  end
+
+  def active_authentication
+    set_flash_message :notice, :signed_up if is_flashing_format?
+    sign_up(resource_name, resource)
+    respond_with resource, location: after_sign_up_path_for(resource)do |format|
+      format.js
+    end
+  end
+
+  def not_active_authentication
+    set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+    expire_data_after_sign_in!
+    respond_with resource, location: after_inactive_sign_up_path_for(resource) do |format|
+      format.js
     end
   end
 
