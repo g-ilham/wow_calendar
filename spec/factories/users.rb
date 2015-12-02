02fae81af8e91eb5269f87cfb0c8b3ac95c270bf
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-12-02 21:22:10 +0300

    [Rspec] added factory for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,4 +2,14 @@
 
 FactoryGirl.define do
   sequence(:email) { |n| "test_user#{n}@example.com" }
+
+  factory :user do
+    first_name "Ivan"
+    last_name "Ivanov"
+    email "test@example.com"
+    password Devise.friendly_token[0,20]
+    confirmed_at DateTime.now
+    photo { Rack::Test::UploadedFile.new(File.join(Rails.root,
+            'spec', 'support', 'images', 'test_user_photo.jpg')) }
+  end
 end
