48532dda5a8d21196c4d86b56b7dcaa9903c5b83
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-12-03 13:49:49 +0300

    [Rspec | helpers] added match to eq helper

--- spec/support/expectation_helper.rb
+++ spec/support/expectation_helper.rb
@@ -14,4 +14,12 @@ module ExpectationHelper
   def expect_to_see_no_in_selector(content)
     expect(page).to_not have_selector(content)
   end
+
+  def to_eq_in_selector(selector, content)
+    expect(selector).to eq(content)
+  end
+
+  def to_no_eq_in_selector(selector, content)
+    expect(selector).to_not eq(content)
+  end
 end
