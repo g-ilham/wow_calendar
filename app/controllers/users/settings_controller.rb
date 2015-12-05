48cd1daaf5601cdb0a46de2b3eaa52754490aab1
g-ilham <ilgamgaysin@gmail.com>
2015-12-05 18:47:10 +0300

    [Refactoring] Settings Controller

--- app/controllers/users/settings_controller.rb
+++ app/controllers/users/settings_controller.rb
@@ -1,33 +1,29 @@
 class Users::SettingsController < ApplicationController
   respond_to :json
-  before_action :get_prev_notifications_options, only: [ :update ]
 
-  expose(:check_changed_notifications_options) do
-    Events::Notifications.new(nil).
-      update_events_notifications!(current_user, @prev_notifications_options)
-  end
-
-  def edit
+  expose(:notifications_generator) do
+    Users::Notifications.new(current_user, @prev_notifications_options)
   end
 
   def update
-    current_user.update(user_params)
-    check_changed_notifications_options
+    @prev_notifications_options = current_user.notifications_options
+
+    if current_user.update(user_params)
+      notifications_generator.run
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email,
-                                :first_name,
-                                :last_name,
-                                :photo,
-                                :in_fifteen_minutes,
-                                :in_hour,
-                                :in_day)
-  end
-
-  def get_prev_notifications_options
-    @prev_notifications_options = current_user.notifications_options
+    params.require(:user).permit(
+      :email,
+      :first_name,
+      :last_name,
+      :photo,
+      :in_fifteen_minutes,
+      :in_hour,
+      :in_day
+    )
   end
 end
---
+++ app/interactors/users/notifications.rb
@@ -0,0 +1,69 @@
+require 'sidekiq/api'
+require_dependency "#{Rails.root}/app/mailers/event_mailer"
+
+class Users::Notifications
+
+  OPTIONS = {
+    in_fifteen_minutes: 'Через пятнадцать минут у вас событие: ',
+    in_hour: 'Через час у вас событие: ',
+    in_day: 'Через день у вас событие: '
+  }
+
+  attr_reader :user,
+              :selected_at_least_of_one_option,
+              :prev_notifications_options
+
+  def initialize(user, prev_notifications_options)
+    self.user = user
+    self.selected_at_least_of_one_option = user.notifications_options.any?(&:present?)
+    self.prev_notifications_options = prev_notifications_options
+  end
+
+  def run
+    run_update_for_collection! if notification_options_updated?
+  end
+
+  private
+
+  def run_update_for_collection!
+    user.events.each do |event|
+      Events::CleanScheduledJobs.new(event.id, 'EventMailer')
+
+      if selected_at_least_of_one_option
+        run_this
+      end
+    end
+  end
+
+  def run_this
+    selected_options.each do |key, value|
+      sending_ops = get_sending_time(key, event)
+
+      if sending_ops > Time.zone.now
+        EventMailer.delay_until(sending_ops)
+                   .notify(event, value)
+      end
+    end
+  end
+
+  def selected_options
+    OPTIONS.select do |k, v|
+      user.public_send(k).present?
+    end
+  end
+
+  def get_sending_time(key, event)
+    case key.to_s
+    when 'in_fifteen_minutes'
+      event.starts_at - 15.minutes
+    when 'in_hour'
+      event.starts_at - 1.hour
+    when 'in_day'
+      event.starts_at - 1.day
+    end
+  end
+
+  def notification_options_updated?
+    user.notifications_options != prev_notifications_options
+  end
+end
