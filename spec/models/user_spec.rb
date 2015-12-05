41ea1d89f53fba9c84f691fd48fb0519f022e99e
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-12-05 13:36:36 +0300

    [Rspec | user model spec] update test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 describe User, type: :model do
 
-  let(:first_name) { "Ilgam" }
-  let(:user) { FactoryGirl.create(:user, first_name: first_name) }
+  let(:user) { FactoryGirl.create(:user, first_name: "Ilgam") }
 
   subject { user }
 
@@ -19,10 +18,12 @@ describe User, type: :model do
       is_at_least(2).is_at_most(100)
   end
 
-  it { is_expected.to allow_value(nil).for(:first_name) }
-  it { is_expected.to allow_value(nil).for(:last_name) }
-  it { is_expected.to allow_value("").for(:first_name) }
-  it { is_expected.to allow_value("").for(:last_name) }
+  it { is_expected.to allow_value("john@example.com", "lambda@gusiev.com").for(:email) }
+  it { is_expected.to_not allow_value("invalid", nil, "a@b", "john@.com").for(:email) }
+  it { is_expected.to allow_value("Таня2", "Tanya 3", nil, "").for(:first_name) }
+  it { is_expected.to allow_value("Таня2", "Tanya 3", nil, "").for(:last_name) }
+  it { is_expected.to_not allow_value("Tany@", "john@.-#com").for(:first_name) }
+  it { is_expected.to_not allow_value("Tany@", "john@.-#com").for(:last_name) }
 
   it "#first_name returns a string" do
     expect(user.first_name).to match "Ilgam"
