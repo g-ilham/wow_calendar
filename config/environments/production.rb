eb4f59587a1f9a6afd1eca8aa0f0f3270dffec91
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-12-19 18:50:58 +0300

    [Mailer] added default url ops to production conf file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = { host: 'http://wowcalendar.herokuapp.com' }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
